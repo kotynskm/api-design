950280095e84761e5353032051a492777f7e03b4
Kailey <k.kotynski@gmail.com>
2023-11-06 11:34:51 -0800

    add validation middleware for routes

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from "./utils/middleware";
 
 const router = Router();
 
@@ -9,9 +11,19 @@ router.get("/product", (req, res) => {
 
 router.get("/product/:id", (req, res) => {});
 
-router.post("/product", (req, res) => {});
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 
-router.put("/product", (req, res) => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 
 router.delete("/product/:id", (req, res) => {});
 
@@ -22,9 +34,21 @@ router.get("/update", (req, res) => {
 
 router.get("/update/:id", (req, res) => {});
 
-router.post("/update", (req, res) => {});
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  (req, res) => {}
+);
 
-router.put("/update", (req, res) => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("version").optional(),
+  (req, res) => {}
+);
 
 router.delete("/update/:id", (req, res) => {});
 
@@ -35,10 +59,21 @@ router.get("/updatepoint", (req, res) => {
 
 router.get("/updatepoint/:id", (req, res) => {});
 
-router.post("/updatepoint", (req, res) => {});
+router.post(
+  "/updatepoint",
+  body("name").exists().isString(),
+  body("description").exists().isString(),
+  body("updateId").exists().isString(),
+  (req, res) => {}
+);
 
-router.put("/updatepoint", (req, res) => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  (req, res) => {}
+);
 
 router.delete("/updatepoint/:id", (req, res) => {});
 
-export default router
+export default router;
